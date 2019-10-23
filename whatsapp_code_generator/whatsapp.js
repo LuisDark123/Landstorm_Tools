2f512ba30ba1fa7ac94fa034e56b663ea35f5819
Luis Falcon <luisdark935@hotmail.com>
2019-10-22 19:38:48 -0500

    Corrección en el funcionamiento del generador de códigos para whatsapp

--- whatsapp_code_generator/whatsapp.js
+++ whatsapp_code_generator/whatsapp.js
@@ -1,10 +1,9 @@
 const tel = document.getElementById('input-tel');
-const message = document.getElementById('input-message').value;
-const 
+const message = document.getElementById('input-message');
 
 const generator = document.getElementById('btn-generator');
 const code = document.getElementById('code');
 
 generator.addEventListener('click', () => {
-    code.innerHTML = `https://api.whatsapp.com/send?phone=${tel.value}&text=${message}`
+    code.innerHTML = `https://api.whatsapp.com/send?phone=${tel.value}&text=${message.value.replace(/ /g, "%20")}`
 });
